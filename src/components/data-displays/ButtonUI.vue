7e7b55064d01580d2d5df3f5ffaae7d66287e343
rlyn <rlynjb@users.noreply.github.com>
2023-10-31 10:01:55 -0700

    Update ButtonUI.vue

--- src/components/data-displays/ButtonUI.vue
+++ src/components/data-displays/ButtonUI.vue
@@ -1,1 +1,37 @@
+<template>
+  <button class="btn rounded-none">
+    <span :class="'icon '+ iconStart" />
+    <span class="text">{{ label }}</span>
+    <span :class="'icon '+ iconEnd" />
+  </button>
+</template>
 
+<script lang="ts">
+export default {
+  name: "Button",
+  props: {
+    label: {
+      type: String,
+      default: "Button Name",
+    },
+    iconStart: {
+      type: String,
+      default: '',
+    },
+    iconEnd: {
+      type: String,
+      default: '',
+    }
+  },
+};
+</script>
+
+<style lang="postcss">
+.btn.btn-link,
+.btn.btn-link:hover {
+  text-decoration-line: none;
+}
+.btn .text {
+  margin: 0 -5px;
+}
+</style>
