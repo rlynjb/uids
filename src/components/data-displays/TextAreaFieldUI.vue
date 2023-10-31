c63bb09b45957d6d3192ea70e55ce81cce1f3dbb
rlyn <rlynjb@users.noreply.github.com>
2023-10-31 10:44:42 -0700

    Create TextAreaFieldUI.vue

---
+++ src/components/data-displays/TextAreaFieldUI.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="textarea-field s-field-container form-control">
+    <label class="label s-field-label">
+      {{ label }}
+    </label>
+    <textarea
+      :value="modelValue"
+      class="textarea textarea-bordered rounded-none"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      rows="6"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    />
+    <p
+      v-if="errorStatus"
+      class="text-primary text-sm"
+    >
+      Required
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * A textarea field using Vue3 Form Input Bindings.
+ *  
+ * @displayName TextAreaFieldUI
+ */
+export default {
+  name: "TextAreaField",
+  props: {
+    label: {
+      type: String,
+      default: 'Sample Label'
+    },
+    placeholder: {
+      type: String,
+      default: 'Type here..'
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    errorStatus: {
+      type: Boolean,
+      default: false,
+    }
+  },
+}
+</script>
+
+<style lang="postcss">
+.s-field-container .s-field-label {
+  padding-left: 0;
+}
+.textarea-field .textarea {
+  line-height: 1.25rem !important;
+}
+textarea.textarea:focus {
+  outline: none;
+}
+</style>
