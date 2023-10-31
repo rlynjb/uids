121a027c3094705525a32d83d4647c3fb6d86345
rlyn <rlynjb@users.noreply.github.com>
2023-10-31 10:43:46 -0700

    Create TextFieldUI.vue

---
+++ src/components/data-displays/TextFieldUI.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="text-field s-field-container"
+  >
+    <div class="form-control w-full">
+      <label
+        v-if="label"
+        class="label s-field-label"
+      >
+        {{ label }}
+      </label>
+      <input
+        :value="modelValue"
+        type="text"
+        class="input input-bordered w-full rounded-none"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      >
+      <p
+        v-if="errorStatus"
+        class="text-primary text-sm"
+      >
+        Required
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * A text field using Vue3 Form Input Bindings.
+ *  
+ * @displayName TextFieldUI
+ */
+export default {
+  name: "TextField",
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: 'Type here..'
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    errorStatus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scope lang="postcss">
+.s-field-container .s-field-label {
+  padding-left: 0;
+}
+.input[type="text"] {
+  font-size: 0.875rem;
+}
+.input[type="text"]:focus {
+  outline: none;
+}
+</style>
