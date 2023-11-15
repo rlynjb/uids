dcb6bc6c094bb4b90d80cb8c2540bb4232b959b6
rlyn <rlynjb@users.noreply.github.com>
2023-11-15 11:06:26 -0800

    Update TextFieldUI.vue - moved code snippet

--- src/components/data-displays/TextFieldUI.vue
+++ src/components/data-displays/TextFieldUI.vue
@@ -74,30 +74,3 @@ export default {
   outline: none;
 }
 </style>
-
-
-<docs lang="md">
-  ##### Basic usage
-  ```js
-  <TextFieldUI
-    label="sample label"
-    placeholder="sample placeholder"
-  />
-  ```
-
-  ##### Set field to disable
-  ```js
-  <TextFieldUI
-    label="sample label"
-    :disabled="true"
-  />
-  ```
-
-  ##### Set error
-  ```js
-  <TextFieldUI
-    label="sample label"
-    :error-status="true"
-  />
-  ```
-</docs>
