dc383c3c10da0e8853ee21b3f4336b0fd1df2558
rlyn <rlynjb@users.noreply.github.com>
2023-11-15 11:07:46 -0800

    Update BreadcrumbUI.vue - removed code snippet

--- src/components/data-displays/BreadcrumbUI.vue
+++ src/components/data-displays/BreadcrumbUI.vue
@@ -83,31 +83,3 @@ export default {
   text-transform: capitalize;
 }
 </style>
-
-
-
-<docs lang="md">
-##### Basic usage
-```js
-<BreadcrumbUI />
-```
-
-##### Set Parent Page
-```js
-/*
-  NOTE:
-  id can be retrieved in store, vue-router, localStorage, whichever you prefer to store routes.
-*/
-const id = 123123
-const paths = [
-  {
-    name: 'Parent Page',
-    path: '/parent-path/{id}/subpage'
-  }
-]
-
-<BreadcrumbUI
-  :parent-paths="paths"
-/>
-```
-</docs>
