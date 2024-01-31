4eed5d38ff23a89a20cf343cb6f07f9038c71dd8
rlyn <rlynjb@users.noreply.github.com>
2024-01-30 16:13:07 -0800

    Create LoaderUI.vue

---
+++ src/components/data-displays/LoaderUI.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="loading loading-spinner loading-lg" />
+</template>
+
+<script lang="ts">
+import { ref, watch } from 'vue'
+
+/**
+ * A simple Loader
+ * 
+ * @displayName LoaderUI
+ */
+export default {
+  name: "ToastUI",
+}
+</script>
+
+<style scoped lang="postcss">
+
+</style>
